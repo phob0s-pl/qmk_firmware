22b1ac091d60014837ab0f686bd227a738ac8fec
phob0s <git@phob0s.pl>
2022-07-02 02:18:13 +0200

    update linux keymap

--- keyboards/crkbd/keymaps/phob0s/keymap.c
+++ keyboards/crkbd/keymaps/phob0s/keymap.c
@@ -4,13 +4,13 @@
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#include "manna-harbour_miryoku.h"
+#include "keymap.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_split_3x6_3(
     KC_ESC, KC_Q,              KC_W,              KC_F,              KC_P,              KC_B,              KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT, RGB_TOG,
     KC_LSFT, LGUI_T(KC_A),      LALT_T(KC_R),      LCTL_T(KC_S),      LSFT_T(KC_T),      KC_G,              KC_M,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_I),      LGUI_T(KC_O), RGB_MOD,
-    KC_LGUI, LT(BUTTON, KC_Z),  ALGR_T(KC_X),      KC_C,              KC_D,              KC_V,              KC_K,              KC_H,              KC_COMM,           ALGR_T(KC_DOT),    LT(BUTTON, KC_SLSH), KC_RALT,
+    LT(LINUX, U_NU), LT(BUTTON, KC_Z),  ALGR_T(KC_X),      KC_C,              KC_D,              KC_V,              KC_K,              KC_H,              KC_COMM,           ALGR_T(KC_DOT),    LT(BUTTON, KC_SLSH), KC_RALT,
                  LT(MEDIA, KC_ESC), LT(NAV, KC_SPC),   LT(MOUSE, KC_TAB), LT(SYM, KC_ENT),   LT(NUM, KC_BSPC),  LT(FUN, KC_DEL)
   ),
   [NAV] = LAYOUT_split_3x6_3(
@@ -54,6 +54,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     U_NA,KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_TRNS, KC_TRNS, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,U_NA,
     U_NA,U_UND,   U_CUT,   U_CPY,   U_PST,   U_RDO,   U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,U_NA,
     KC_BTN2, KC_BTN3, KC_BTN1, KC_BTN1, KC_BTN3, KC_BTN2
+  ),
+  [LINUX] = LAYOUT_split_3x6_3(
+    RGUI(KC_GRAVE),RGUI(KC_1),   RGUI(KC_2),   RGUI(KC_3),   RGUI(KC_4),   RGUI(KC_5),   RGUI(KC_6),   RGUI(KC_7),   RGUI(KC_8),   RGUI(KC_9),   U_NA, U_NA,
+    U_NA, U_NA,      U_NA,      U_NA,      RGUI(KC_F),      U_NA,              RGUI(KC_H),              RGUI(KC_J),      RGUI(KC_K),      RGUI(KC_L),      RGUI(KC_SEMICOLON), U_NA,
+    U_NA, U_NA,  U_NA,      U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,           U_NA,    U_NA, U_NA,
+    U_NA, RGUI(KC_SPC), U_NA, RGUI(KC_ENT), U_NA, U_NA
   )
 };
 
